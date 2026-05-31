67472b8c0864ea18a615684f278935b94f85f500
Adpros7 <pranit.advik@gmail.com>
2026-05-30 23:46:05 -0400

    detail in prompt

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ def make_stl(output_file_with_extension: str, input_file_with_extension: str):
     print(r.stdout)
     return r.stdout
 
+generate_parts = Agent("Generate-parts", "Use this when u want to generate a new high detail part", [search_docs, WebSearchTool()], instructions="You are a expert detailer who specializes in making OpenScad parts. you are a part of a whole. use the docs and the web to help you. make no no misakes. be very detailed. be very realistic. o genric rounded corner, i want every detail to amaze the user. return OpenSCAD code")
+
 
 instruction = """ You are a master OpenSCAD designer who uses programming to create stunning 3d models
 Use web search to find anything not in docs
@@ -66,12 +68,13 @@ and use make_stl to output a stl
 Work until the model works
 Output a stl unless the user specifies otherwise
 Unless otherwise specified, create the most high-detailed, amzing, superb model the world has ever seen, it should haave insasenley good detailing, down to the little nooks and crannies
+It should also be really big so you can put details on it unless otherwise specified
 """
 
 enable_verbose_stdout_logging()
 agent = Agent(
     "OpenSCAD-Designer",
-    tools=[search_docs, WebSearchTool(), write_file, make_stl],
+    tools=[search_docs, WebSearchTool(), write_file, make_stl, generate_parts.as_tool("GenerateParts", "Use when u want to generate highly realistic or detailed parts")],
     instructions=instruction,
 )
 
